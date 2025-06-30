4d34ff3772b0dea9743aa55abd0844362239bad7
NERD <168670760+zhiern@users.noreply.github.com>
2025-06-30 09:18:39 +0800

    移动openlist至网络存储

--- scripts/diy-x86_64.sh
+++ scripts/diy-x86_64.sh
@@ -365,6 +365,7 @@ git clone https://$github/sbwml/openwrt-alist package/new/alist
 
 # openlist
 git clone https://$github/sbwml/luci-app-openlist package/new/openlist
+sed -i 's/\"services\"/\"nas\"/g' package/new/openlist/luci-app-openlist/root/usr/share/luci/menu.d/luci-app-openlist.json
 
 # luci-app-sqm
 rm -rf feeds/luci/applications/luci-app-sqm
