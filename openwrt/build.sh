226d0b908675fb5ebbf6a9ed2f95fc4a80246f51
NERD <168670760+zhiern@users.noreply.github.com>
2025-08-08 12:51:36 +0800

    Update build.sh

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -275,7 +275,7 @@ elif [ "$platform" = "rockchip" ]; then
 fi
 
 # config-common
-curl -s $mirror/openwrt/24-config-common > .config >> .config
+curl -s $mirror/openwrt/24-config-common >> .config
 
 # ota
 [ "$ENABLE_OTA" = "y" ] && [ "$version" = "v24" ] && echo 'CONFIG_PACKAGE_luci-app-ota=y' >> .config
